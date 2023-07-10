3d383828e34db3f1cdac66f48dfa6ea18727fa94
Saatwik <saatwik.sy@gmail.com>
2023-07-10 08:14:41 +0530

    Color picker UI

--- webviews/shared/custom_properties/ColorPicker.svelte
+++ webviews/shared/custom_properties/ColorPicker.svelte
@@ -1,1 +1,59 @@
-<!-- <h1>ColorPicker</h1> -->
+<script lang="ts">
+    import type { ColorPickerProperties } from "../../model/custom_properties";
+
+    export let properties: ColorPickerProperties;
+
+    function handleColorChange(event) {
+        properties.color = event.target.value;
+    }
+</script>
+
+<div class="container">
+    <span>{properties.title}</span>
+    <div class="color-picker">
+        <input 
+            class="color-input" 
+            type="color" 
+            bind:value="{properties.color}" 
+            on:input="{handleColorChange}">
+        <span>{properties.color != "" ? properties.color : "Choose a color"}</span>
+    </div>
+</div>
+
+<style lang="scss">
+    @import "../ui//variables.scss";
+
+    .container {
+        margin: 10px 0 10px 0;
+
+        .color-picker {
+            margin-top: 8px;
+            display: flex;
+            align-items: center;
+
+            .color-input {
+                border-radius: 50%;
+                height: 40px;
+                width: 40px;
+                border: none;
+                outline: none;
+                padding: 0;
+                cursor: pointer;
+            }
+
+            .color-input::-webkit-color-swatch-wrapper {
+                padding: 0; 
+            }
+            .color-input::-webkit-color-swatch {
+                border: none;
+                border-radius: 50%;
+            }
+
+            span {
+                margin-left: 10px;
+                user-select: none;
+                cursor: pointer;
+            }
+        }
+    }
+</style>
