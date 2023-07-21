7e7996116e07275477ac76d0f2662a939bd652bb
Saatwik <saatwik.sy@gmail.com>
2023-07-21 12:52:51 +0530

    Webview object parsed

--- components/model/text_properties.ts
+++ components/model/text_properties.ts
@@ -1,15 +1,15 @@
 import { type Base } from "./base";
 
 export interface CodeTextProperties extends Base {
-    text: string,
-    maxLines: string,
-    textAlign: string,
-    style: Style
+    text?: string,
+    maxLines?: string,
+    textAlign?: string,
+    style?: Style
 }
 
 interface Style extends Base {
-    fontSize: string,
-    color: string,
-    fontWeight: string,
-    styling: string,
+    fontSize?: string,
+    color?: string,
+    fontWeight?: string,
+    styling?: string,
 }
--- src/generateCode.ts
+++ src/generateCode.ts
@@ -1,7 +1,33 @@
 import type { CodeTemplate } from "../common/code";
+import { replacableCharacter } from "../common/constants";
+import { CodeTextProperties } from "../components/model/text_properties";
+import { text } from "../components/text";
 
 export function generateCode(object: CodeTemplate) {
-    for (var key in object) {
-        
+    let finalObject: CodeTextProperties = parseObjectWithProps(object);
+    return convertObjectToCode(finalObject);
+}
+
+function parseObjectWithProps(object: CodeTemplate): CodeTextProperties {
+    console.log(object);
+    let allProps: CodeTextProperties = text;
+    allProps = updateProps(allProps, object);
+    return allProps;
+}
+
+function updateProps(props, object) {
+    for (var key in props) {
+        if (typeof(props[key]) === 'string') {
+            if (object.hasOwnProperty(key)) {
+                props[key] = props[key].replace(replacableCharacter, object[key]);
+            }
+        } else {
+            props[key] = updateProps(props[key], object);
+        }
     }
+    return props;
+}
+
+function convertObjectToCode(object: CodeTextProperties): string {
+    return "";
 }
